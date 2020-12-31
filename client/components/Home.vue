96efb49d14e6c9272a90b603a7cba8139fbbcbca
youssefJendoubi <73699133+youssefJendoubi@users.noreply.github.com>
2020-12-31 19:25:46 +0100

    JY

--- client/components/Home.vue
+++ client/components/Home.vue
@@ -6,18 +6,32 @@
         <h2>A propos de Step By Step</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, totam accusantium voluptatibus laudantium ipsa reiciendis quae corrupti sapiente! Consectetur, debitis fugit asperiores excepturi consequatur eos! Et, beatae! Nobis, quaerat eius!</p>
       </div>
+      <div class="scroll down"></div>
+       <a href="#sec-2">
+         <div class="scroll-down"></div>
+        </a>
     </section >
-    <section id="sec-3">
+    <section id="sec-2">
       <div class="subsection">
         <h2>Qui sommes nous ?</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, totam accusantium voluptatibus laudantium ipsa reiciendis quae corrupti sapiente! Consectetur, debitis fugit asperiores excepturi consequatur eos! Et, beatae! Nobis, quaerat eius!</p>
       </div>
+      <div class="scroll down"></div>
+      <a href="#sec-3">
+        <div class="scroll-down"></div>
+      </a>
     </section >
-    <section id="sec-2">
+    <section id="sec-3">
       <div class="subsection">
         <h2>Nos algorithmes</h2>
       </div>
         <algo-tpl :algorithms="algorithms"></algo-tpl>
+
+      <div class="scroll down"></div>
+      <a href="#sec-1">
+        <div class="scroll-down"></div>
+      </a>
+
     </section >
     
    
@@ -48,6 +62,7 @@ module.exports = {
   margin: 0;
   padding: 0;
   font-family: 'Poppins', sans-serif;
+  
 }
 
 .header {
@@ -58,6 +73,94 @@ module.exports = {
   text-align: center;
   padding: 10% 20% 0% 20%;
 }
+        html {
+            scroll-behavior: smooth;
+        }
+        
+        body {
+            
+            height: 100vh;
+            background-size: cover;
+            background-position: center;
+        }
+        
+        
+       
+        section {
+            display: inline;
+        }
+        
+        
+        section {
+            min-height: 100vh;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        
+        #sec-1 {
+            position: relative
+        }
+        
+        #sec-2 {
+            
+            position: relative;
+        }
+        
+        #sec-3 {
+            
+            position: relative;
+        }
+        
+        
+        .scroll-down {
+            height: 50px;
+            width: 30px;
+            border: 2px solid rgb(0, 0, 0);
+            position: absolute;
+            left: 50%;
+            bottom: 20px;
+            border-radius: 50px;
+            cursor: pointer;
+        }
+        
+        .scroll-down::before,
+        .scroll-down::after {
+            content: "";
+            position: absolute;
+            top: 20%;
+            left: 50%;
+            height: 10px;
+            width: 10px;
+            transform: translate(-50%, -100%) rotate(45deg);
+            border: 2px solid rgb(0, 0, 0);
+            border-top: transparent;
+            border-left: transparent;
+            animation: scroll-down 1s ease-in-out infinite;
+        }
+        
+        .scroll-down::before {
+            top: 30%;
+            animation-delay: 0.3s;
+        }
+        
+        @keyframes scroll-down {
+            0% {
+                opacity: 0;
+            }
+            30% {
+                opacity: 1;
+            }
+            60% {
+                opacity: 1;
+            }
+            100% {
+                top: 90%;
+                opacity: 0;
+            }
+        }
+
 
 
 
