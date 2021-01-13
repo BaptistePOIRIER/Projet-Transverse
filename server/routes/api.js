ddc516754d16f654e3c27f4d1b0535a686238b09
Theticman <bpoirier.341@gmail.com>
2021-01-14 00:48:50 +0100

    Updating register / login

--- server/routes/api.js
+++ server/routes/api.js
@@ -19,66 +19,91 @@ client.connect()
  * Cette route permet d'inscrire un utilisateur
  */
 router.post('/register', async (req,res) => {
-    const email = req.body.email
-    const password = req.body.password
-  
-    // Récupération des email existants
-    const result = await client.query({
-      text: 'SELECT email FROM users'
-    })
-  
-    // Email déjà utilisé dans la base de donnée ?
-    const user_email = result.rows.find(a => a.email === email)
-    if (user_email) {
-      res.status(400).json({ message: 'Email already used'})
-      return
-    }
-  
-    // Hashage du mot de passe
-    const hash = await bcrypt.hash(password, 10)
-  
-    // Stockage de l'utilisateur
-    client.query({
-      text: 'INSERT INTO users(name,email,password) VALUES ($1,$2)',
-      values: [email,hash]
-    })
-    res.status(200).json({ message: 'Successfully registered'})
+  const name = req.body.name
+  const email = req.body.email
+  const password = req.body.password
+
+  // Récupération des noms existants
+  const result_name = await client.query({
+    text: 'SELECT name FROM users'
   })
   
-  /**
-   * Cette route permet à l'utilisateur de se connecter
-   */
-  router.post('/login', async (req,res) => {
-    const email = req.body.email
-    const password = req.body.password
-  
-    // Récupération des email existants
-    const result = await client.query({
-      text: 'SELECT email,password,id FROM users'
-    })
-  
-    // Utilisateurs absent de la base de donnée ?
-    const user = result.rows.find(a => a.email === email)
-    if (!user) {
-      res.status(400).json({ message: 'Email cannot be found'})
-      return
-    }
-  
-    // Vérification du mot de passe
-    if (await bcrypt.compare(password, user.password)) {
-      // Déjà connecté
-      if(req.session.userId == user.id) {
-        res.status(401).json({ message: 'Already connected'})
-      }else {
-        // Connexion
-        req.session.userId = user.id
-        res.status(200).json({ message: 'Successfully connected'})
-      }
-      return
-    }
-    // Mauvais mot de passe
-    res.status(400).json({ message: 'Wrong password'})
+  // Nom déjà utilisé dans la base de donnée ?
+  const user_name = result_name.rows.find(a => a.name === name)
+  if (user_name) {
+    res.status(400).json({ message: 'Nom déjà utilisé'})
+    return
+  }
+
+  // Récupération des email existants
+  const result_email = await client.query({
+    text: 'SELECT email FROM users'
   })
+  
+  // Email n'a pas le bon format
+  if (!/\S+@\S+\.\S+/.test(email)) {
+    res.status(400).json({ message: `Le format de l'email est incorrect`})
+    return
+  }
+
+  // Email déjà utilisé dans la base de donnée ?
+  const user_email = result_email.rows.find(a => a.email === email)
+  if (user_email) {
+    res.status(401).json({ message: 'Email déjà utilisé'})
+    return
+  }
+
+  // Mot de passe trop faible
+  if (password.length < 4) {
+    res.status(401).json({ message: 'Mot de passe trop simple'})
+    return
+  }
+
+  // Hashage du mot de passe
+  const hash = await bcrypt.hash(password, 10)
+  
+  // Stockage de l'utilisateur
+  client.query({
+    text: 'INSERT INTO users(name,email,password) VALUES ($1,$2,$3)',
+    values: [name,email,hash]
+  })
+  res.status(200).json({ message: 'Enregistré avec succès'})
+})
+  
+/**
+ * Cette route permet à l'utilisateur de se connecter
+ */
+router.post('/login', async (req,res) => {
+  const email = req.body.email
+  const password = req.body.password
+  
+  // Récupération des email existants
+  const result = await client.query({
+    text: 'SELECT email,password,id FROM users'
+  })
+  
+  // Utilisateurs absent de la base de donnée ?
+  const user = result.rows.find(a => a.email === email)
+  if (!user) {
+    res.status(400).json({ message: 'Email introuvable'})
+    return
+  }
+  
+  // Vérification du mot de passe
+  if (await bcrypt.compare(password, user.password)) {
+    // Déjà connecté
+    if(req.session.userId == user.id) {
+      res.status(401).json({ message: 'Déjà connecté'})
+    }else {
+      // Connexion
+      req.session.userId = user.id
+      res.status(200).json({ message: 'Connecté avec succès'})
+    }
+    return
+  }
+  // Mauvais mot de passe
+  res.status(400).json({ message: 'Mauvais mot de passe'})
+})
 
 /**
 * Cette route permet de renvoyer l'utilisateur connecté
@@ -86,13 +111,13 @@ router.post('/register', async (req,res) => {
 router.get('/me', async (req,res) => {
   // Connecté ?
   if (typeof req.session.userId !== 'number') {
-    res.status(401).send({ message: 'No connected users' })
+    res.status(401).send({ message: `Vous n'êtes pas connecté` })
     return
   }
 
   // Récupération des email existants
   const result = await client.query({
-    text: 'SELECT email,id FROM users'
+    text: 'SELECT name,email,id FROM users'
   })
 
   // Récupération de l'utilisateur connecté
